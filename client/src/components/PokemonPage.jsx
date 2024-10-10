a58609c0e1c7bc3ce1b824aef5e5fd5da2b43de4
JojorioCh <josephch@missionreadyhq.com>
2024-10-10 23:52:20 +1300

    removed all functions

--- client/src/components/PokemonPage.jsx
+++ client/src/components/PokemonPage.jsx
@@ -3,64 +3,34 @@ import PokemonCard from "./PokemonCard";
 
 export default function PokemonPage() {
   // set up the states needed for the fetched data and the caught status
-  const [pokemonData, setPokemonData] = useState("");
-  const [caughtStatus, setCaughtStatus] = useState("");
+  const [first, setfirst] = useState(second);
+  const [first1, setfirst1] = useState(second);
 
   // fetch the data from your backend - use the urls you have created
   // complete the useEffect function to help fetch the data from the backend
   useEffect(() => {
     async function fetchPokemonData() {
-      const response = await fetch("http://localhost:4000/pokemonlist");
-      const data = await response.json();
-      setPokemonData(data);
+      
     }
     fetchPokemonData();
-    console.log(pokemonData);
   }, []);
 
   // Complete the handleCaught function to send a POST request to the server
-  async function handleCaught(pokemonId) {
-    if (caughtStatus[pokemonId]) {
-      try {
-        const response = await fetch("http://localhost:4000/pokemonlist", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ id: pokemonId }),
-        });
-        const data = await response.json();
-        console.log("response from server:", data);
-        // reload page after catching pokemon
-        setPokemonData(
-          pokemonData.filter((pokemon) => pokemon.id !== pokemonId)
-        );
-      } catch (error) {
-        console.error("Error updating Pokemon:", error);
-      }
-    }
-    console.log("Pokemon caught!");
+  async function handleCaught() {
+
   }
 
-  // handle the checkbox
-  function handleChange(e, pokemonId) {
-    setCaughtStatus((prevState) => ({
-      ...prevState,
-      [pokemonId]: e.target.checked,
-    }));
-    console.log(pokemonId);
-  }
+  // handle the checkbox change so that the caught status can be updated
+  function handleChange() {
 
+  }
+  // finish the function for the input and the button
   return (
     <div className="pokemon-page-container">
       {pokemonData.length > 0 ? (
         pokemonData.map((pokemon, index) => (
           <div className="pokemans-container" key={index}>
-            <input
-              type="checkbox"
-              checked={caughtStatus[pokemon.id] || false}
-              onChange={(e) => handleChange(e, pokemon.id)}
-            />
+            <input type="checkbox" onChange={handleChange} />
             <PokemonCard
               name={pokemon.name}
               type={pokemon.type}
@@ -68,9 +38,7 @@ export default function PokemonPage() {
               weight={pokemon.weight}
               src={pokemon.image}
             />
-            <button
-              className="caught-btn"
-              onClick={() => handleCaught(pokemon.id)}>
+            <button className="caught-btn" onClick={handleCaught}>
               Caught 'Em!
             </button>
           </div>
